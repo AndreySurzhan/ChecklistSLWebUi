322e25501784b5be1b6e8d7d6fa64c02e24130ee
Andrei.Surzhan <surzhan.a.y@gmail.com>
2019-01-23 13:19:11 -0500

    Styling User Summary

--- src/components/NavBlock.js
+++ src/components/NavBlock.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import UserSummary from './UserSummary';
 import ChecklistsBlock from './ChecklistsBlock';
+import Divider from '@material-ui/core/Divider';
 
 class NavBlock extends React.Component {
     render() {
         return (
             <div className={this.props.className}>
                 <UserSummary user={this.props.user} />
+                <Divider />
                 <ChecklistsBlock checklists={this.props.checklists}/>
             </div>
         );
--- src/components/UserSummary.js
+++ src/components/UserSummary.js
@@ -3,7 +3,12 @@ import Avatar from '@material-ui/core/Avatar';
 
 class UserSummary extends React.Component {
     render() {
-        return <Avatar alt="User's Profile Summary">A</Avatar>;
+        return (
+            <div style={{height: '15vh'}}>
+                <Avatar alt="User's Profile Summary">A</Avatar>
+            </div>
+
+        );
     }
 }
 
